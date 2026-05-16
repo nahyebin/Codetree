91826f320af6c32d7bea0433a803d3961512c62d
nahyebin <164003217+nahyebin@users.noreply.github.com>
2026-05-16 14:44:48 +0900

    [Passed] 수의 계절은 (44ms, 16MB)

---
+++ trail1/수의 계절은/season-of-num.py
@@ -0,0 +1,10 @@
+a = int(input())
+
+if a==3 or a==4 or a==5:
+    print("Spring")
+elif a==6 or a==7 or a==8:
+    print("Summer")
+elif a==9 or a==10 or a==11:
+    print("Fall")
+else:
+    print("Winter")
