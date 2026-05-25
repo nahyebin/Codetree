28997ae1ac26a15fb9ab9c70cc050d6b2567ee6a
nahyebin <164003217+nahyebin@users.noreply.github.com>
2026-05-25 19:17:32 +0900

    [Passed] 숫자의 증감 (47ms, 16MB)

--- trail1/숫자의 증감/increasing-and-decreasing-numbers.py
+++ trail1/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -2,10 +2,14 @@ c, n = input().split()
 n = int(n)
 
 if c == "A" :
-    for i in range (n):
-        i=i+1
+    # for i in range (n):
+    #     i=i+1
+    #     print(i, end=" ")
+    for i in range (1, n+1):
         print(i, end=" ")
 else:
-    for i in range (n):
-        print(n, end=" ")
-        n=n-1
+    # for i in range (n):
+    #     print(n, end=" ")
+    #     n=n-1
+    for i in range(n,0,-1):
+        print(i, end=" ")
