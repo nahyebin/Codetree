65e0ecabec15965d9329a9b054a904ecfb9ec82d
nahyebin <164003217+nahyebin@users.noreply.github.com>
2026-05-28 23:50:33 +0900

    [Wrong Answer] 청소당번 정하기 (49ms, 16MB)

---
+++ trail1/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+bathroom=0
+classroom=0
+way=0
+
+for i in range(1,n,1):
+    if i % 12 == 0:
+        bathroom=bathroom+1
+    elif i % 3 == 0:
+        way=way+1
+    elif i % 2 == 0:
+        classroom=classroom+1
+
+print(classroom, way, bathroom)
+        
